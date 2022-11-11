4d11b34d8dcd29e26ee2ef560020b1cffeafee9d
Remil <51848204+RemilRLs@users.noreply.github.com>
2022-11-11 06:42:18 +0100

    fix mobile responsive

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,6 @@
 const primaryNav = document.querySelector('.menu-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+const navShow = document.querySelector('.navbar');
 
 navToggle.addEventListener('click', () => { // We listen if there is a click on the button.
     const visibility = primaryNav.getAttribute('data-visible'); // We listen the attribute of the menu-navigation.
@@ -13,3 +14,20 @@ navToggle.addEventListener('click', () => { // We listen if there is a click on 
         navToggle.setAttribute('aria-expanded', false);
     }
 }); 
+
+primaryNav.addEventListener('scroll', () =>{ // We check if the user scroll.
+
+})
+
+var prevScrollpos = window.pageYOffset;
+
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+        navShow.setAttribute('data-visible', false);
+  } else {
+        navShow.setAttribute('data-visible', true);
+  } 
+  prevScrollpos = currentScrollPos;
+}
